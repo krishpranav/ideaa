955974ab502b839c2ae1c2faceebb9685a2b4259
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-21 11:47:16 +0530

    src: utils[string]

---
+++ src/utils/string.js
@@ -0,0 +1,16 @@
+const strings = {
+    appName: 'Idea Application',
+    setCalendarDate: 'Select date & time to set a reminder',
+    saveToCalendar: 'Save To calendar',
+    toastConfirmation: 'Event booked on your Calendar',
+    savedIdeas: 'Saved Ideas',
+    gitHub: 'GitHub Repo',
+    reminderDeleted: 'Reminder removed from your Calendar',
+    markAsDone: 'Marked as completed',
+    noActiveEvent: 'No Active Events',
+    completedEvents: 'Completed Events',
+    deniedCalendar: 'Calendar permission denied',
+    goToSettings: 'Go to Settings',
+};
+  
+export default strings;
