802faee6a13bfb9ab4626be2a1d7b646b19f4db7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-21 11:49:19 +0530

    redux: actions

---
+++ src/redux/actions.js
@@ -0,0 +1,18 @@
+export const SET_EVENT = 'SET_EVENT';
+export const UPDATE_EVENT = 'UPDATE_EVENT';
+export const SET_CALENDAR_PERMISSION = 'SET_CALENDAR_PERMISSION';
+
+export const saveEvent = response => dispatch => {
+    dispatch({
+        type: SET_EVENT,
+        payload: response,
+    });
+};
+
+export const setCalendarPermission = response => dispatch => {
+    dispatch({
+        type: SET_CALENDAR_PERMISSION,
+        payload: response,
+    });
+};
+
